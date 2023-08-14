70d854c718842ad45359b3f28c7978649405a684
bigansh <anshpushti2001@gmail.com>
2023-08-14 22:32:33 +0530

    fix name update

--- server/functions/lifecycle/chatQueryAndUpdate.js
+++ server/functions/lifecycle/chatQueryAndUpdate.js
@@ -16,20 +16,22 @@ const chatQueryAndUpdate = async (chatQueryObject) => {
 
 		if (!foundChat) throw new Error('No chat with that Chat ID exists.')
 
-		const promptResult = await promptQuery(
-			chatQueryObject.prompt,
-			foundChat[0]
-		)
+		if (chatQueryObject.prompt) {
+			const promptResult = await promptQuery(
+				chatQueryObject.prompt,
+				foundChat[0]
+			)
 
-		chatQueryObject.chat_history = `${foundChat[0].chat_history}\n\nuser input: ${chatQueryObject.prompt}\ngenerated result: ${promptResult.response}`
-		chatQueryObject.chat_array = [
-			...foundChat[0].chat_array,
-			{ user: chatQueryObject.prompt, llm: promptResult.response },
-		]
-		chatQueryObject.source_documents = [
-			...new Set(promptResult.sourceDocumentIds),
-		]
-		chatQueryObject.prompt = undefined
+			chatQueryObject.chat_history = `${foundChat[0].chat_history}\n\nuser input: ${chatQueryObject.prompt}\ngenerated result: ${promptResult.response}`
+			chatQueryObject.chat_array = [
+				...foundChat[0].chat_array,
+				{ user: chatQueryObject.prompt, llm: promptResult.response },
+			]
+			chatQueryObject.source_documents = [
+				...new Set(promptResult.sourceDocumentIds),
+			]
+			chatQueryObject.prompt = undefined
+		}
 
 		return await updateChat(chatQueryObject)
 	} catch (error) {
