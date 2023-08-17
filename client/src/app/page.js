a85b1fe59a95389daefc997298185b32f62404d1
iHarryD <anuraagvr555@gmail.com>
2023-08-17 18:50:28 +0530

    feat: add static section on home

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,18 +1,11 @@
 'use client'
 
-import {
-	Heading,
-	Text,
-	Flex,
-	Button,
-	useColorMode,
-	Box,
-	useColorModeValue,
-} from '@chakra-ui/react'
-import { Head } from 'next/document'
+import { Button, Container, Flex, Text, useColorMode } from '@chakra-ui/react'
 import Script from 'next/script'
-import { useEffect, useState } from 'react'
-import { motion } from 'framer-motion'
+import { useEffect, useRef, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { Footer } from '@/components'
+import { useColors } from '@/utils/colors'
 
 const scrollingTexts = [
 	'imagine not having to read your bookmarks...',
@@ -21,14 +14,24 @@ const scrollingTexts = [
 ]
 
 export default function Home() {
-	const { toggleColorMode } = useColorMode()
+	const { base700, text, base600 } = useColors()
 	const [focusedTextIndex, setFocusedTextIndex] = useState(0)
+	const [isPastScrollingText, setIsPastScrollingText] = useState(false)
+	const staticSectionRef = useRef(null)
 
 	useEffect(() => {
 		function scrollHandler(e) {
-			setFocusedTextIndex(
-				Math.round(e.target.defaultView.scrollY / window.innerHeight)
+			const indexToSet = Math.round(
+				e.target.defaultView.scrollY / window.innerHeight
 			)
+			if (!isPastScrollingText && indexToSet >= scrollingTexts.length) {
+				const sectionFromTop =
+					staticSectionRef.current?.getClientRects()?.[0].top
+				if (sectionFromTop <= 0) {
+					setIsPastScrollingText(true)
+				}
+			}
+			setFocusedTextIndex(indexToSet)
 		}
 		window.addEventListener('scroll', scrollHandler)
 		return () => window.removeEventListener('scroll', scrollHandler)
@@ -36,72 +39,99 @@ export default function Home() {
 
 	return (
 		<>
-			<section
-				data-w-id='38731044-e467-6d7d-8b84-2e54494432a0'
-				class='section wf-section'
-			>
-				<div class='div-block'>
-					<div
-						class='w-layout-blockcontainer container w-container'
-						style={{ maxWidth: 'unset' }}
+			<AnimatePresence>
+				{!isPastScrollingText ? (
+					<motion.section
+						data-w-id='38731044-e467-6d7d-8b84-2e54494432a0'
+						class='section wf-section'
+						exit={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
 					>
-						<div
-							class='div-block-2'
-							style={{
-								display: 'flex',
-								flexDirection: 'column',
-								gap: 32,
-							}}
-						>
-							{/* <div class='text _1'></div>
-							<div class='text _2'>
-								imagine not having to read your bookmarks...
-							</div>
-							<div class='text _3'>
-								imagine not having to read your bookmarks...
-							</div> */}
-							{scrollingTexts.map((text, index) => (
-								<motion.p
-									animate={{
-										y: `-${focusedTextIndex * 100}%`,
-										opacity:
-											index > focusedTextIndex
-												? 0
-												: focusedTextIndex - index === 1
-												? 0.75
-												: focusedTextIndex - index === 2
-												? 0.5
-												: 1,
-										transition: {
-											type: 'just',
-										},
-									}}
+						<div class='div-block'>
+							<div
+								class='w-layout-blockcontainer container w-container'
+								style={{ maxWidth: 'unset' }}
+							>
+								<div
+									class='div-block-2'
 									style={{
-										fontSize:
-											focusedTextIndex - index === 1
-												? 24
-												: focusedTextIndex - index === 2
-												? 20
-												: 26,
-										display:
-											index > focusedTextIndex
-												? 'none'
-												: undefined,
+										display: 'flex',
+										flexDirection: 'column',
+										gap: 32,
 									}}
 								>
-									{text}
-								</motion.p>
-							))}
+									{scrollingTexts.map((text, index) => (
+										<motion.p
+											animate={{
+												y: `-${
+													focusedTextIndex * 100
+												}%`,
+												opacity:
+													index > focusedTextIndex
+														? 0
+														: focusedTextIndex -
+																index ===
+														  1
+														? 0.75
+														: focusedTextIndex -
+																index ===
+														  2
+														? 0.5
+														: 1,
+												transition: {
+													type: 'just',
+												},
+											}}
+											style={{
+												fontSize:
+													focusedTextIndex - index ===
+													1
+														? 24
+														: focusedTextIndex -
+																index ===
+														  2
+														? 20
+														: 26,
+												display:
+													index > focusedTextIndex
+														? 'none'
+														: undefined,
+											}}
+										>
+											{text}
+										</motion.p>
+									))}
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			</section>
-			<section class='section-2 wf-section'>
-				<div>This is some text inside of a div block.</div>
-				<a href='#' class='w-button'>
-					Button Text
-				</a>
-			</section>
+					</motion.section>
+				) : null}
+			</AnimatePresence>
+			<Flex
+				ref={staticSectionRef}
+				minHeight='100vh'
+				flexDirection='column'
+				alignItems='center'
+				justifyContent='center'
+			>
+				<Text fontSize='larger' color='black' marginBottom='2'>
+					now stop imagining...
+				</Text>
+				<Button
+					title='login'
+					fontWeight={'400'}
+					bg={base700}
+					_hover={{
+						bg: base600,
+					}}
+					color={text}
+					cursor={'pointer'}
+					mt={2}
+				>
+					lets get started
+				</Button>
+			</Flex>
+			<Footer />
 			<Script>Webflow.require(‘ix2’).init()</Script>
 		</>
 	)
