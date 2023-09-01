3648f3b1410df92576366fa95ee510b441a81c12
Nada <nadaaofficial19@gmail.com>
2023-09-01 18:24:56 +0530

    heading fix

--- client/src/app/app/dashboard/components/RightSideBarDrawer.js
+++ client/src/app/app/dashboard/components/RightSideBarDrawer.js
@@ -145,7 +145,7 @@ const RightSideBarDrawer = ({ isOpenDrawer, onCloseDrawer }) => {
 		.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
 	const filteredDocuments = docData
 		?.filter((e) =>
-			e.heading.toLowerCase().includes(documentInput.toLowerCase())
+			e.heading?.toLowerCase().includes(documentInput.toLowerCase())
 		)
 		.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
 	return (
@@ -330,7 +330,7 @@ const RightSideBarDrawer = ({ isOpenDrawer, onCloseDrawer }) => {
 								>
 									<DocumentIcon fill={text} />
 									<Text textAlign={'initial'} isTruncated>
-										{item.heading}
+										{item?.heading}
 									</Text>
 								</Button>
 							))}
