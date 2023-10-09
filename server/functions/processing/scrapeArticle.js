8789c5f943f5a65419a9553558f4a744f8f54b1e
bigansh <anshpushti2001@gmail.com>
2023-10-09 12:39:19 +0530

    remove the useragents

--- server/functions/processing/scrapeArticle.js
+++ server/functions/processing/scrapeArticle.js
@@ -9,22 +9,7 @@ import xss from 'xss'
  */
 const scrapeArticle = async (url) => {
 	try {
-		const data = await extract(
-			url,
-			{},
-			{
-				headers: {
-					Accept: 'text/html, application/xhtml+xml, application/xml;q=0.9, image/webp, */*;q=0.8',
-					'Accept-Encoding': 'gzip, deflate',
-					'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-					'Keep-Alive': 'timeout=5, mix=10',
-					Dnt: '1',
-					'Upgrade-Insecure-Requests': '1',
-					'User-Agent':
-						'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36',
-				},
-			}
-		)
+		const data = await extract(url)
 
 		const content = xss(data?.content)
 		const title = xss(data?.title)
