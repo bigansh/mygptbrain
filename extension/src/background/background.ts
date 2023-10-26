2439ef1c1bb7749e04b96c54fb221fd4b6184432
Nada <nadaaofficial19@gmail.com>
2023-10-26 12:57:23 +0530

    extension

---
+++ extension/src/background/background.ts
@@ -0,0 +1,99 @@
+import { scrapeLink, getUser, getLinks } from "../components/API"
+import { getStoredLinks, getStoredOptions, setStoredLinks, setStoredOptions, setStoredUser } from "../utils/storage"
+
+
+chrome.runtime.onInstalled.addListener(async () => {
+
+  function checkCookie() {
+    return new Promise((resolve, reject) => {
+      chrome.cookies.get({
+        url: "http://testing.mygptbrain.com/",
+        name: "x-session-token",
+      }, (cookie) => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(cookie);
+        }
+      });
+    });
+  }
+
+  try {
+    const cookie = await checkCookie();
+
+    if (!cookie) {
+      chrome.tabs.create({ url: 'https://testing.mygptbrain.com/onboarding/login' });
+    } else {
+      setStoredUser({});
+      setStoredLinks([]);
+      chrome.action.setBadgeBackgroundColor({ color: '#50C878' });
+
+      const user = await getUser().then(res => res.json());
+
+      const links = await getLinks({ profileId: user.profile_id }).then(res => res.json());
+
+      setStoredUser(user);
+      setStoredLinks(links.filter(link => link.documentMetadata.url !== null));
+    }
+  } catch (err) {
+    console.log(err, "Error checking cookie or fetching user data");
+  }
+});
+
+chrome.runtime.onMessage.addListener(
+  async (res) => {
+    // opens on extension icon click
+    // this runtime gets message from the tab icon (popup.tsx)
+    const { tab } = res.payload
+
+    if (res.message === 'ADD_TO_DOCUMENTS') {
+      chrome.action.setBadgeText({ text: ' ', tabId: tab.id });
+      chrome.action.setBadgeBackgroundColor(
+        { color: '#50C878' },
+      );
+
+      try {
+        const link = await scrapeLink(tab.url).then(res => res.json()).catch(err => console.log(err, 'here'))
+        getStoredLinks().then((links: any) => {
+          setStoredLinks([...links, link])
+        })
+
+      } catch (err) {
+        console.log(err, "err user");
+      }
+    }
+  })
+//fires on tab change
+chrome.tabs.onActivated.addListener(
+
+  async ({ tabId }) => {
+    const tab = await getCurrentTab(tabId)
+    const links: any = await getStoredLinks()
+    const isCurrentTabBookmarked = links.find(e => e.documentMetadata.url == tab.url)
+    if (isCurrentTabBookmarked) {
+      chrome.action.setBadgeText({ text: ' ', tabId: tab.id });
+    }
+  }
+
+)
+
+//fires on internal url change
+chrome.tabs.onUpdated.addListener(
+  async (tabId, changeInfo, tab) => {
+    if (changeInfo.status == "complete") {
+      const links: any = await getStoredLinks()
+      const isCurrentTabBookmarked = links.find(e => e.documentMetadata.url == tab.url)
+      if (isCurrentTabBookmarked) {
+        chrome.action.setBadgeText({ text: ' ', tabId: tab.id });
+      }
+    }
+  },
+)
+
+
+async function getCurrentTab(tabId) {
+  let tab = await chrome.tabs.get(tabId)
+  return tab;
+}
+
---
+++ extension/src/popup/popup.tsx
@@ -0,0 +1,166 @@
+//@ts-nocheck
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import {
+  ChakraProvider,
+  Heading,
+  Text,
+  Flex,
+  Image,
+  Button,
+  Link,
+} from "@chakra-ui/react";
+import "./popup.css";
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/work-sans";
+import {
+  getStoredLinks,
+  getStoredOptions,
+} from "../utils/storage";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Work Sans', sans-serif`,
+    body: `'Work Sans', sans-serif`,
+  },
+  styles: {
+    global: () => ({
+      html: {
+        bg: "",
+        color: "",
+      },
+      body: {
+        bg: "",
+        color: "",
+      },
+    }),
+  },
+});
+
+const Popup: React.FC<{}> = () => {
+  const [options, setOptions] = useState(null);
+  const [cookie, setCookie] = useState<any>();
+  const [bookmark, setBookmark] = useState({
+    id: "",
+    heading: "",
+    description: "",
+  });
+
+  chrome.storage.onChanged.addListener((changes, namespace) => {
+
+    if (changes.links?.newValue) {
+      if (changes.links.newValue.length !== changes.links.oldValue.length) {
+        const currentLink = links?.newValue.filter(
+          ({ id: id1 }) =>
+            !changes?.links?.oldValue.some(({ id: id2 }) => id2 === id1)
+        )[0];
+
+        setBookmark(currentLink);
+      } else {
+
+        setBookmark(changes.links.newValue.find((b) => b.id == bookmark.id));
+      }
+    }
+  });
+
+  useEffect(() => {
+    saveLinks();
+    getStoredOptions().then((options: any) => {
+      setOptions(options);
+    });
+  }, []);
+
+  const saveLinks = () => {
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true,
+      },
+      (tabs) => {
+        if (tabs.length > 0) {
+          getStoredLinks().then((links: any) => {
+            const isSaved = links.find(
+              (e) => e.documentMetadata.url == tabs[0].url
+            );
+
+            if (!isSaved) {
+
+              chrome.runtime.sendMessage({
+                message: "ADD_TO_DOCUMENTS",
+                payload: { tab: tabs[0] },
+              });
+
+            } else {
+
+              setBookmark({
+                id: isSaved.document_id,
+                heading: isSaved?.heading,
+                description: isSaved?.description,
+              });
+            }
+          });
+        }
+      }
+    );
+  };
+
+  const getCookie = async () => {
+    await chrome.cookies
+      .get({
+        url: "http://testing.mygptbrain.com/",
+        name: "x-session-token",
+      })
+      .then((c) => {
+        if (c?.value) {
+          setCookie(c?.value);
+        } else {
+          chrome.tabs.query(
+            {
+              active: true,
+              currentWindow: true,
+            },
+            (tabs) => {
+              if (tabs.length > 0) {
+                const redirectUrl = encodeURIComponent(tabs[0].url);
+                chrome.tabs.create({
+                  url: `http://testing.mygptbrain.com/onboarding/login?redirect=${redirectUrl}`,
+                });
+              }
+            }
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err, "err");
+        if (err?.message == "The cookie is missing a required attribute.") {
+          chrome.tabs.create({
+            url: `http://testing.mygptbrain.com/onboarding/login?redirect=${chrome.runtime.getURL()}`,
+          });
+        }
+      });
+  };
+
+  getCookie();
+
+  return (
+    <ChakraProvider theme={theme}>
+      {cookie && (
+        <Flex className="popup" direction="column" gap={4} m={6}>
+          <Heading lineHeight={1} fontWeight={400} fontSize="2xl">
+            Bookmark Saved!
+          </Heading>
+          <Text fontSize="md" noOfLines={3} textTransform="capitalize">
+            {bookmark?.heading}
+          </Text>
+          <Text fontSize="sm" noOfLines={3}>
+            {bookmark?.description}
+          </Text>
+        </Flex>
+      )}
+    </ChakraProvider>
+  );
+};
+
+const root = document.createElement("div");
+document.body.appendChild(root);
+ReactDOM.render(<Popup />, root);
